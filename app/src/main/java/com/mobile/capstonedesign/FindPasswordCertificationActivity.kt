039d026c11cdeb8ce7ee9ce44f49627b98584318
KoreanCodingMachine <pon03078@naver.com>
2020-05-17 21:57:19 +0900

    [200517] jh modified

--- app/src/main/java/com/mobile/capstonedesign/FindPasswordCertificationActivity.kt
+++ app/src/main/java/com/mobile/capstonedesign/FindPasswordCertificationActivity.kt
@@ -1,7 +1,10 @@
 package com.mobile.capstonedesign
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_find_password_certification.*
+
 
 
 
@@ -9,6 +12,11 @@ class FindPasswordCertificationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fing_password_certification)
+        setContentView(R.layout.activity_find_password_certification)
+
+        FindPasswordCertification.setOnClickListener {
+            val intent = Intent(this,FindPasswordInputActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
